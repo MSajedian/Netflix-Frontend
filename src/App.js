a1db9e6202a006130cb23fa35d72d71373e1d5f1
MSajedian <mohammadsajedian@gmail.com>
2021-05-05 15:45:26 +0100

    ShowDetail updated

--- src/App.js
+++ src/App.js
@@ -8,64 +8,6 @@ import ShowDetail from "./components/ShowDetail";
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      harryPotterMovies: [],
-      spiderManMovies: [],
-      starWarsMovies: [],
-      searchedMovies: [],
-      loading: true,
-      error: false,
-      comments: [],
-    };
-  }
-
-  url = "http://www.omdbapi.com/?apikey=c71a553d";
-
-  componentDidMount = () => {
-    Promise.all([
-      fetch(this.url + "&s=harry%20potter")
-        .then((response) => response.json())
-        .then((responseObject) =>
-          this.setState({ harryPotterMovies: responseObject.Search })
-        ),
-      fetch(this.url + "&s=spider%20man")
-        .then((response) => response.json())
-        .then((responseObject) =>
-          this.setState({ spiderManMovies: responseObject.Search })
-        ),
-      fetch(this.url + "&s=star%20wars")
-        .then((response) => response.json())
-        .then((responseObject) =>
-          this.setState({ starWarsMovies: responseObject.Search })
-        ),
-    ])
-      .then(() => this.setState({ loading: false }))
-      .catch((err) => {
-        this.setState({ error: true });
-        console.log("An error has occurred:", err);
-      });
-  };
-
-  showSearchResult = (searchString) => {
-    fetch(this.url + "&s=" + searchString)
-      .then((response) => response.json())
-      .then((responseObject) =>
-        this.setState({ searchedMovies: responseObject.Search })
-      );
-  };
-
-  fetchComments = async (movieID) => {
-    const commentsUrl = "https://striveschool.herokuapp.com/api/comments/";
-    const comments = await fetch(commentsUrl + movieID, {
-      headers: new Headers({
-        Authorization: "[INSERT_YOUR_AUTH_HERE]",
-      }),
-    }).then((response) => response.json());
-    this.setState({ comments });
-  };
-
   render() {
     return (
       <div className="App">
--- src/components/ShowDetail.jsx
+++ src/components/ShowDetail.jsx
@@ -1,42 +1,36 @@
 import React, { Component } from "react";
 import { Container, Row } from "react-bootstrap";
-import Movie from "./Movie";
-
 
 class ShowDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
             id: "",
-            Movie: []
+            Movie: {}
         }
     }
 
-
     componentDidMount() {
-        let idFromTheURLBar = this.props.match.params.movieId
+        let idFromTheURLBar = this.props.match.params.movieId.toString()
         console.log('idFromTheURLBar:', idFromTheURLBar)
-        this.fetchMovie()
-        console.log('this.state.Movie:', this.state.Movie)
-
+        this.fetchMovie(idFromTheURLBar)
         // let foundDish = items.find(dish => dish.id.toString() === idFromTheURLBar)
+
         // if (foundDish) {
         //     console.log(foundDish)
-        //     this.setState({
-        //         dishToShow: foundDish
-        //     })
+        // this.setState({ dishToShow: foundDish })
         // }
     }
 
     url = "http://www.omdbapi.com/?apikey=c71a553d";
 
-    fetchMovie = async () => {
+    fetchMovie = async (idFromTheURLBar) => {
         try {
-            let response = await fetch(this.url + "&s=harry%20potter")
+            let response = await fetch(`${this.url}&i=${idFromTheURLBar}`)
             if (response.ok) {
                 let data = await response.json()
-                console.log("data.Search", data.Search)
-                this.setState({ Movie: data.Search })
+                console.log("data", data)
+                this.setState({ Movie: data })
             } else {
                 console.log("Error")
             }
@@ -46,24 +40,6 @@ class ShowDetail extends Component {
         }
     }
 
-    // fetchMovie2 = () => {
-    //     fetch(this.url + "&s=harry%20potter")
-    //         .then((response) => {
-    //             return response.json()
-    //         })
-    //         .then((responseObject) => {
-    //             console.log(responseObject.Search);
-    //             return this.setState({ Movie: responseObject.Search })
-    //         }
-    //         )
-    //         .then(() => this.setState({ loading: false }))
-    //         .catch((err) => {
-    //             this.setState({ error: true });
-    //             console.log("An error has occurred:", err);
-    //         });
-    // }
-
-
 
     render() {
         return (
@@ -72,16 +48,12 @@ class ShowDetail extends Component {
                     <h1 className="text-white">ShowDetail</h1>
                 </Row>
                 <Row className="justify-content-center">
-                    {
-                        this.state.Movie.map((movie) => (
-                            <Movie
-                                data={movie}
-                                key={movie.imdbID}
-                                // comments={comments}
-                                // fetchComments={fetchComments}
-                            />
-                        ))
-                    }
+                    {this.state.Movie && (
+                        <div>
+                            <img src={this.state.Movie.Poster} alt={this.state.Movie.Title} />
+                            <div className="text-white text-center">{this.state.Movie.Title}</div>
+                        </div>
+                    )}
                 </Row>
             </Container>
         );
