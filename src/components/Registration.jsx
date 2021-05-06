b4c8ef0b8505532eba0e4964fddf69f219699491
MSajedian <mohammadsajedian@gmail.com>
2021-05-06 15:34:03 +0100

    update form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,99 +1,252 @@
-import React, { Component } from 'react';
-import { Form, InputGroup, Col, Button, Container } from "react-bootstrap";
+import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
+// Schema for yup
+const validationSchema = Yup.object().shape({
+    // Name - Required, at least 2 chars
+    firstName: Yup.string()
+        .min(2, "*First Names must have at least 2 characters")
+        .max(100, "*First Names can't be longer than 100 characters")
+        .required("*First Name is required"),
 
-class Registration extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { validated: false }
-    }
+    // Surname - Required, at least 3 chars
+    surname: Yup.string()
+        .min(3, "*surnames must have at least 3 characters")
+        .max(100, "*surnames can't be longer than 100 characters")
+        .required("*surname is required"),
 
-    handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
+    // Email - Required - Should be an email field
+    email: Yup.string()
+        .email("*Must be a valid email address")
+        .max(100, "*Email must be less than 100 characters")
+        .required("*Email is required"),
 
-        this.setState({ validated: true });
-    };
-    render() {
-        return (
-            <Container>
-                <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
-                    <Form.Row>
-                        <Form.Group as={Col} md="4" controlId="validationCustom01">
-                            <Form.Label className="text-white">First name</Form.Label>
+    // Password - Required - Should contain at least 8 chars, 1 digit, 1 letter
+    password: Yup.string()
+        .min(8, "*Password must have at least 8 characters")
+        .max(100, "*Password must be less than 100 characters")
+        .required("*Password is required"),
+
+    // Year of Birth - Required - from 1910+
+    yearofBirth: Yup.string()
+        .required("*Password is required"),
+
+    // Street Address - Required
+    streetAddress: Yup.string()
+        .required("*Password is required"),
+
+    // City - Required
+    city: Yup.string()
+    .required("*Password is required"),
+
+    // Postal Code - Required - Numeric 5 digits
+    postalCode: Yup.string()
+    .required("*Password is required"),
+
+    // Credit card - XXXX-XXXX-XXXX-XXXX (EXTRA)
+    creditCard: Yup.string()
+    .required("*Password is required"),
+
+});
+
+// phone: Yup.string()
+//     .matches(phoneRegExp, "*Phone number is not valid")
+//     .required("*Phone number required"),
+// blog: Yup.string()
+//     .url("*Must enter URL in http://www.example.com format")
+//     .required("*URL required")
+
+const Registration = () => {
+    return (
+        <Container>
+            {/* //Sets initial values for form inputs */}
+            <Formik
+                initialValues={{ firstName: "", surname: "", email: "", password: "", yearofBirth: 1910, streetAddress: "", city: '', postalCode: '', creditCard: 0 }}
+                validationSchema={validationSchema}
+                onSubmit={(values, { setSubmitting, resetForm }) => {
+                    // When Button submits form and form is in the process of submitting, submit Button is disabled
+                    setSubmitting(true);
+
+                    // Simulate submitting to database, shows us values submitted, resets form
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        resetForm();
+                        setSubmitting(false);
+                    }, 500);
+                }}
+            >
+                {/* Callback function containing Formik state and helpers that handle common form actions */}
+                {({ values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting }) => (
+                    <Form onSubmit={handleSubmit} className="mx-auto">
+                        {console.log(values)}
+                        <Form.Group controlId="formFirstName">
+                            <Form.Label className="text-white">First Name :</Form.Label>
                             <Form.Control
-                                required
                                 type="text"
-                                placeholder="First name"
+                                /* This name property is used to access the value of the form element via values.nameOfElement */
+                                name="firstName"
+                                placeholder="First Name"
+                                /* Set onChange to handleChange */
+                                onChange={handleChange}
+                                /* Set onBlur to handleBlur */
+                                onBlur={handleBlur}
+                                /* Store the value of this input in values.name, make sure this is named the same as the name property on the form element */
+                                value={values.firstName}
+                                /* Check if the name field (this field) has been touched and if there is an error, if so add the .error class styles defined in the CSS (make the input box red) */
+                                className={touched.firstName && errors.firstName ? "error" : null}
                             />
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            {/* Applies the proper error message from validateSchema when the user has clicked the element and there is an error, also applies the .error-message CSS class for styling */}
+                            {touched.firstName && errors.firstName ? (
+                                <div className="error-message">{errors.firstName}</div>
+                            ) : null}
                         </Form.Group>
-                        <Form.Group as={Col} md="4" controlId="validationCustom02">
-                            <Form.Label className="text-white">Last name</Form.Label>
+                        <Form.Group controlId="formSurname">
+                            <Form.Label className="text-white">Surname :</Form.Label>
                             <Form.Control
-                                required
                                 type="text"
-                                placeholder="Last name"
+                                /* This name property is used to access the value of the form element via values.nameOfElement */
+                                name="surname"
+                                placeholder="Surname"
+                                /* Set onChange to handleChange */
+                                onChange={handleChange}
+                                /* Set onBlur to handleBlur */
+                                onBlur={handleBlur}
+                                /* Store the value of this input in values.name, make sure this is named the same as the name property on the form element */
+                                value={values.surname}
+                                /* Check if the name field (this field) has been touched and if there is an error, if so add the .error class styles defined in the CSS (make the input box red) */
+                                className={touched.surname && errors.surname ? "error" : null}
                             />
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            {/* Applies the proper error message from validateSchema when the user has clicked the element and there is an error, also applies the .error-message CSS class for styling */}
+                            {touched.surname && errors.surname ? (
+                                <div className="error-message">{errors.surname}</div>
+                            ) : null}
                         </Form.Group>
-                        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-                            <Form.Label className="text-white">Username</Form.Label>
-                            <InputGroup hasValidation>
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Username"
-                                    aria-describedby="inputGroupPrepend"
-                                    required
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    Please choose a username.
-                    </Form.Control.Feedback>
-                            </InputGroup>
+                        <Form.Group controlId="formEmail">
+                            <Form.Label className="text-white">Email :</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="email"
+                                placeholder="Email"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.email}
+                                className={touched.email && errors.email ? "error" : null}
+                            />
+                            {touched.email && errors.email ? (
+                                <div className="error-message">{errors.email}</div>
+                            ) : null}
                         </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
-                        <Form.Group as={Col} md="6" controlId="validationCustom03">
-                            <Form.Label className="text-white">City</Form.Label>
-                            <Form.Control type="text" placeholder="City" required />
-                            <Form.Control.Feedback type="invalid">
-                                Please provide a valid city.
-                  </Form.Control.Feedback>
+                        <Form.Group controlId="formpassword">
+                            <Form.Label className="text-white">Password :</Form.Label>
+                            <Form.Control
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.password}
+                                className={touched.password && errors.password ? "error" : null}
+                            />
+                            {touched.password && errors.password ? (
+                                <div className="error-message">{errors.password}</div>
+                            ) : null}
                         </Form.Group>
-                        <Form.Group as={Col} md="3" controlId="validationCustom04">
-                            <Form.Label className="text-white">State</Form.Label>
-                            <Form.Control type="text" placeholder="State" required />
-                            <Form.Control.Feedback type="invalid">
-                                Please provide a valid state.
-                  </Form.Control.Feedback>
+                        <Form.Group controlId="formyearofBirth">
+                            <Form.Label className="text-white">Year of Birth :</Form.Label>
+                            <Form.Control
+                                type="number"
+                                name="yearofBirth"
+                                placeholder="Year of Birth"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.yearofBirth}
+                                className={touched.yearofBirth && errors.yearofBirth ? "error" : null}
+                            />
+                            {touched.yearofBirth && errors.yearofBirth ? (
+                                <div className="error-message">{errors.yearofBirth}</div>
+                            ) : null}
                         </Form.Group>
-                        <Form.Group as={Col} md="3" controlId="validationCustom05">
-                            <Form.Label className="text-white">Zip</Form.Label>
-                            <Form.Control type="text" placeholder="Zip" required />
-                            <Form.Control.Feedback type="invalid">
-                                Please provide a valid zip.
-                  </Form.Control.Feedback>
+
+                        <Form.Group controlId="formstreetAddress">
+                            <Form.Label className="text-white">Street Address :</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="streetAddress"
+                                placeholder="Street Address"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.streetAddress}
+                                className={touched.streetAddress && errors.streetAddress ? "error" : null}
+                            />
+                            {touched.streetAddress && errors.streetAddress ? (
+                                <div className="error-message">{errors.streetAddress}</div>
+                            ) : null}
                         </Form.Group>
-                    </Form.Row>
-                    <Form.Group>
-                        <Form.Check
-                            className="text-white"
-                            required
-                            label="Agree to terms and conditions"
-                            feedback="You must agree before submitting."
-                        />
-                    </Form.Group>
-                    <Button type="submit">Submit form</Button>
-                </Form>
-            </Container>
-        );
-    }
+
+                        <Form.Group controlId="formcity">
+                            <Form.Label className="text-white">City :</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="city"
+                                placeholder="City"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.city}
+                                className={touched.city && errors.city ? "error" : null}
+                            />
+                            {touched.city && errors.city ? (
+                                <div className="error-message">{errors.city}</div>
+                            ) : null}
+                        </Form.Group>
+
+                        <Form.Group controlId="formpostalCode">
+                            <Form.Label className="text-white">Postal Code :</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="postalCode"
+                                placeholder="Postal Code"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.postalCode}
+                                className={touched.postalCode && errors.postalCode ? "error" : null}
+                            />
+                            {touched.postalCode && errors.postalCode ? (
+                                <div className="error-message">{errors.postalCode}</div>
+                            ) : null}
+                        </Form.Group>
+
+                        <Form.Group controlId="formcreditCard">
+                            <Form.Label className="text-white">Credit Card :</Form.Label>
+                            <Form.Control
+                                type="number"
+                                name="creditCard"
+                                placeholder="Credit Card"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.creditCard}
+                                className={touched.creditCard && errors.creditCard ? "error" : null}
+                            />
+                            {touched.creditCard && errors.creditCard ? (
+                                <div className="error-message">{errors.creditCard}</div>
+                            ) : null}
+                        </Form.Group>
+
+
+                        <Button variant="primary" type="submit" disabled={isSubmitting}> Submit </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Container>
+    );
 }
+
 
 export default Registration;
